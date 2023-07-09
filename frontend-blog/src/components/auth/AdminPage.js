a39f2deac1d812ec946aca378a299d1b56e81bc2
Crystalsawers <sawece1@student.op.ac.nz>
2023-07-09 17:50:49 +1200

    create admin page component

---
+++ frontend-blog/src/components/auth/AdminPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AdminPage = () => {
+    return (
+      <div>
+        {/* Your admin page content goes here */}
+      </div>
+    );
+  };
+  
+  export default AdminPage;
+  
