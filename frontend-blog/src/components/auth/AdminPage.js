7f29add11f4aa16be3f9e093d6a5625078785d71
Crystalsawers <sawece1@student.op.ac.nz>
2023-07-09 18:01:35 +1200

    add admin page

--- frontend-blog/src/components/auth/AdminPage.js
+++ frontend-blog/src/components/auth/AdminPage.js
@@ -1,12 +1,72 @@
 import React from 'react';
 
 const AdminPage = () => {
-    return (
-      <div>
-        {/* Your admin page content goes here */}
-      </div>
-    );
+
+  const handleCreateBlogPost = () => {
+    // Logic to create a blog post
   };
+
+  const handleUpdateBlogPost = () => {
+    // Logic to update a blog post
+  };
+
+  const handleDeleteBlogPost = () => {
+    // Logic to delete a blog post
+  };
+
+  const handleCreateComment = () => {
+    // Logic to create a comment
+  };
+
+  const handleUpdateComment = () => {
+    // Logic to update a comment
+  };
+
+  const handleDeleteComment = () => {
+    // Logic to delete a comment
+  };
+
+  const handleCreateCategory = () => {
+    // Logic to create a category
+  };
+
+  const handleUpdateCategory = () => {
+    // Logic to update a category
+  };
+
+  const handleDeleteCategory = () => {
+    // Logic to delete a category
+  };
+
+  return (
+    <div>
+      <h2>Admin Page</h2>
+      <h3>Blog Posts</h3>
+      <button onClick={handleCreateBlogPost}>Create Blog Post</button>
+      <button onClick={handleUpdateBlogPost}>Update Blog Post</button>
+      <button onClick={handleDeleteBlogPost}>Delete Blog Post</button>
+
+      <h3>Comments</h3>
+      <button onClick={handleCreateComment}>Create Comment</button>
+      <button onClick={handleUpdateComment}>Update Comment</button>
+      <button onClick={handleDeleteComment}>Delete Comment</button>
+
+      <h3>Categories</h3>
+      <button onClick={handleCreateCategory}>Create Category</button>
+      <button onClick={handleUpdateCategory}>Update Category</button>
+      <button onClick={handleDeleteCategory}>Delete Category</button>
+
+      <h3>Profiles</h3>
+      {/* Display all profiles, but no editing or deleting functionality */}
+      
+      <h3>Users</h3>
+      {/* Display all users in the database */}
+    </div>
+  );
+};
+
+
+
   
   export default AdminPage;
   
