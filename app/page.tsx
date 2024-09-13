a9aaebbb918d3d430fa94bc82b61ff917c5637d8
Crystalsawers <sawece1@student.op.ac.nz>
2024-09-14 09:29:00 +1200

    feat:move about desc into about page

--- app/page.tsx
+++ app/page.tsx
@@ -1,53 +1,16 @@
 "use client";
-import { useEffect, useState } from 'react';
-import { collection, getDocs, DocumentData, QuerySnapshot } from 'firebase/firestore';
-import { db } from '../lib/firebaseConfig';
+// import { useEffect, useState } from 'react';
+// import { collection, getDocs, DocumentData, QuerySnapshot } from 'firebase/firestore';
+// import { db } from '../lib/firebaseConfig';
 
-// Define the data type based on your Firestore structure
-interface AboutMeDocument {
-  introduction: string;
-  personal_story: string;
-  contact_info: {
-    email: string;
-    linkedin: string;
-    twitter: string;
-  };
-}
+
 
 const Home = () => {
-  const [data, setData] = useState<AboutMeDocument[]>([]);
-
-  // Fetch data from Firestore
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const querySnapshot: QuerySnapshot<DocumentData> = await getDocs(collection(db, 'about-me'));
-        const items: AboutMeDocument[] = querySnapshot.docs.map(doc => doc.data() as AboutMeDocument);
-        setData(items);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
   return (
-    <div>
-      <h1>About Me</h1>
-      {data.map((item, index) => (
-        <div key={index}>
-          <h2>Introduction</h2>
-          <p>{item.introduction}</p>
-          <h2>Personal Story</h2>
-          <p>{item.personal_story}</p>
-          <h2>Contact Info</h2>
-          <p>Email: {item.contact_info.email}</p>
-          <p>LinkedIn: <a href={item.contact_info.linkedin} target="_blank" rel="noopener noreferrer">{item.contact_info.linkedin}</a></p>
-          <p>Twitter: <a href={item.contact_info.twitter} target="_blank" rel="noopener noreferrer">{item.contact_info.twitter}</a></p>
-        </div>
-      ))}
-    </div>
+    <main>
+      <h1>Coming Soon</h1>
+      <p>We are working hard to bring the latest blog posts to the home page. Stay tuned!</p>
+    </main>
   );
 };
 
