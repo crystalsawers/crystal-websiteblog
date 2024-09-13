524d5903412be07545deebedba0182c32bd08545
Crystalsawers <sawece1@student.op.ac.nz>
2024-09-14 10:31:17 +1200

    styling medis responsive

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,19 +4,18 @@ import Logo from './cs-high-resolution-logo-transparent.png'; // personal logo f
 
 const Navbar = () => {
   return (
-    <nav className="pb-4 border-b-2 border-gray-700 flex items-center gap-5 my-10 mx-auto max-w-5xl">
-      <Image
-        src={Logo}
-        alt="Crystal Sawers logo"
-        width={70}
-        placeholder="blur"
-        quality={100}
-      />
-      <h1 className="text-[var(--navbar-text)]">Crystal&#39;s Blog</h1>
+    <nav className="navbar">
+      <Link href="/" className="flex items-center space-x-2">
+        <Image
+          src={Logo}
+          alt="Crystal Sawers logo"
+          width={70}
+          placeholder="blur"
+          quality={100}
+        />
+        <h1 className="text-[var(--navbar-text)]">Crystal&#39;s Blog</h1>
+      </Link>
       <div className="flex space-x-4">
-        <Link href="/" className="text-[var(--navbar-text)] hover:text-white">
-          Dashboard
-        </Link>
         <Link href="/about" className="text-[var(--navbar-text)] hover:text-white">
           About
         </Link>
