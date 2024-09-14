a76c56469c82cde18aa69b783dd6de804f71dd70
Crystalsawers <sawece1@student.op.ac.nz>
2024-09-14 14:54:39 +1200

    edit firebase stuff

--- lib/firebaseConfig.ts
+++ lib/firebaseConfig.ts
@@ -1,14 +1,15 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, doc, setDoc } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDTGgQE4lvX3YdF9fb9CJ1gd7mExhOBiyA",
-  authDomain: "crystal-personalblog.firebaseapp.com",
-  projectId: "crystal-personalblog",
-  storageBucket: "crystal-personalblog.appspot.com",
-  messagingSenderId: "1098650212487",
-  appId: "1:1098650212487:web:c9ef6022359895099b4022",
-  measurementId: "G-JL48ZM0W6Z"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
@@ -17,5 +18,7 @@ const app = initializeApp(firebaseConfig);
 // Initialize Firestore
 const db = getFirestore(app);
 
-export { db, collection, doc, setDoc };
+// Initialize Firebase Auth
+const auth = getAuth(app);
 
+export { db, collection, doc, setDoc, auth };
