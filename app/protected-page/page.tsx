50500e50d6c4aba2e3d2f21ab6f4462983207fa7
Crystalsawers <sawece1@student.op.ac.nz>
2024-09-18 11:22:09 +1200

    update

--- app/protected-page/page.tsx
+++ app/protected-page/page.tsx
@@ -1,12 +1,12 @@
-// app/protected-page/page.tsx
 const ProtectedPage = () => {
-    return (
-      <div>
-        <h1>Welcome back Crystal!</h1>
-        <p>If you&#39;re seeing this you&#39;re logged in. You should see the logout button and option to edit posts</p>
-      </div>
-    );
-  };
-  
-  export default ProtectedPage;
-  
+  return (
+    <div className="text-center">
+      <h1 className="text-custom-green">Welcome back Crystal!</h1>
+      <p className="text-custom-green">
+        If you&#39;re seeing this you&#39;re logged in. You should see the logout button and option to edit posts
+      </p>
+    </div>
+  );
+};
+
+export default ProtectedPage;
