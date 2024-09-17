83646cc0e1f71302ca71d394cb12e7ba9fe5eff5
Crystalsawers <sawece1@student.op.ac.nz>
2024-09-17 18:19:33 +1200

    fix: posts not sorting by most recent

--- lib/utils/sortPostsByDate.ts
+++ lib/utils/sortPostsByDate.ts
@@ -1,9 +1,12 @@
-export function sortPostsByDate<T extends { date?: string }>(posts: T[], dateField: keyof T, timeZone: string = 'Pacific/Auckland'): T[] {
+export function sortPostsByDate<T extends { date?: string }>(posts: T[], dateField: keyof T): T[] {
   return posts
     .filter(post => post[dateField])
     .sort((a, b) => {
-      const dateA = new Date(new Date(a[dateField] as string).toLocaleString('en-GB', { timeZone }));
-      const dateB = new Date(new Date(b[dateField] as string).toLocaleString('en-GB', { timeZone }));
-      return dateB.getTime() - dateA.getTime(); // Sort by latest date first
+      const dateA = new Date(a[dateField] as string);
+      const dateB = new Date(b[dateField] as string);
+
+      // console.log('Comparing dates:', a[dateField], dateA, 'vs', b[dateField], dateB);
+
+      return dateB.getTime() - dateA.getTime(); // Sort by latest date first, already formatted in NZ time by formatDate
     });
 }
